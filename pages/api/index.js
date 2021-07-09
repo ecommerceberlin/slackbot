ee1428e894af83fdda4a4d7e7dbc60f1aaad78bb
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-07-09 23:52:05 +0200

    test

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,15 +1,47 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import {App} from '@slack/bolt'
+import {App, ExpressReceiver} from '@slack/bolt'
+const receiver = new ExpressReceiver({ signingSecret: process.env.SLACK_SIGNING_SECRET });
+
+
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
 
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  receiver,
+  token: process.env.SLACK_BOT_TOKEN
 });
 
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
 
-  
+
+  app.command('/stats', async ({ command, ack, say }) => {
+    await ack();
+    await say(`>>2> ${command.text}`);
+});
+
+
+  await runMiddleware(req, res, receiver.router)
+
 
   res.status(200).json({ name: 'John Doe' })
+}
+
+
+
+export const config = {
+  api: {
+    externalResolver: true,
+  },
 }
